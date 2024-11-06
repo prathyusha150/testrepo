33d6d9592538b2d41c655d69ac4e628a7ea86c7f
prathyusha150 <prathyushaboinapally29@gmail.com>
2024-11-06 09:36:37 +0530

    added addition.java file

---
+++ Addition.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+import java.lang.System;
+class Addition{
+ public static void main(String[] args){
+   int a=10;
+   int b=20;
+   int c=a+b;
+  System.out.println("addition of two numbers="+c);
+}
+}
+ 
